eab39b99733d9f8c5097e00b6266dd971945bb78
AustinRD <austinrd95@gmail.com>
2019-10-03 11:03:10 -0400

    changes 9/23

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app
+    .use('/static', express.static(path.join(__dirname, '../NoFramework')))
+
+    .get(   '/',
+            (req, res) => res.send('Hello New Paltz!!!')
+        )
+    .get(   '/another',
+            function(req, res)
+            {
+                res.send('Another World!')
+            }
+        )
+
+app.listen(port, () => console.log(`Running on http://localhost:${port}`));
